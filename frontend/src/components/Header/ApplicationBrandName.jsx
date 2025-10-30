b4cce5ef1436341a480d1965015b75a3174acb4f
Drax1020 <bajrangirdn6@gmail.com>
2025-10-31 00:45:14 +0530

    new commit

--- frontend/src/components/Header/ApplicationBrandName.jsx
+++ frontend/src/components/Header/ApplicationBrandName.jsx
@@ -7,7 +7,7 @@ const BrandName = memo(() => {
       to="/"
       className="text-2xl font-extrabold text-indigo-600 dark:text-indigo-400 hover:text-indigo-700 dark:hover:text-indigo-300 transition-colors duration-200"
     >
-      Kontributor
+      Contributor
     </Link>
   );
 });
